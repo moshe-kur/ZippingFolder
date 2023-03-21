47f6d42861c72c0402bf46cd589e4a104ede37cc
moshe <105060304+moshe-kur@users.noreply.github.com>
2023-03-21 12:21:41 +0200

    Initial commit

---
+++ Entities/Zipping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Zipping
+    {
+		public static void ZipFolder(string sourcePath, string destinationPath)
+		{
+			try
+			{
+				destinationPath += ".zip";
+				ZipFile.CreateFromDirectory(sourcePath, destinationPath);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				throw;
+			}
+
+		}
+
+	}
+}
---
+++ ZippingFolder/Form1.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZippingFolder
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            if (textBox1.Text=="")
+            {
+                
+            }
+            else
+            {
+				Zipping.ZipFolder(textBox1.Text, textBox1.Text);
+            }
+        }
+	}
+}
